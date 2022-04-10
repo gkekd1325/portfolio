f8ca2ed2466b53efaeb5c7a4c7c1e82b4f641e62
gkekd1325 <gkekd1325@naver.com>
2022-04-10 11:46:48 +0900

    samlip

---
+++ samlip/js/script.js
@@ -0,0 +1,45 @@
+var arr=[
+    "img/slide1.jpg",
+    "img/slide2.jpg",
+    "img/slide3.jpg",
+    "img/slide4.jpg",
+    "img/slide5.jpg"
+]
+var i=0;
+var left =document.querySelector(".left");
+var right=document.querySelector(".right");
+var slide=document.querySelector("#slide");
+
+left.addEventListener("click", reverse);
+right.addEventListener("click", move);
+window.addEventListener("load", interVal);
+
+function move(){
+    // ani();
+    if(i==4){
+        i=-1; /* 더해서 0이 되어야 한다. */
+    }
+    i++;
+    slide.style.transition="0.6s";
+    slide.style.opacity="0";
+    slide.style.background="url("+arr[i]+")";
+    slide.style.backgroundSize="cover";
+    slide.style.opacity="1";
+}
+function reverse(){
+    if(i==0){
+        i=5;
+    }
+    i--;
+    slide.style.transition="0.6s";
+    slide.style.opacity="0";
+    slide.style.background="url("+arr[i]+")";
+    slide.style.backgroundSize="cover";
+    slide.style.opacity="1";
+    
+}
+
+function interVal(){
+    setInterval(move, 6000); /* 6초마다 move함수 실행 */
+}
+
