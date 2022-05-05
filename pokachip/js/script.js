9f8af8adcaf5c77136fd11be878da326c70161a1
gkekd1325 <gkekd1325@naver.com>
2022-05-05 22:50:18 +0900

    pokachip

--- pokachip/js/script.js
+++ pokachip/js/script.js
@@ -1,6 +1,10 @@
 window.addEventListener("load", trans);
+window.addEventListener("scroll", scroll)
 
 function trans(){
     $(".head li").addClass("on");
     $(".logo").addClass("on");
+}
+function scroll(){
+    console.log(scrollY);
 }
