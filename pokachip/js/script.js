2bd41b249b5ff1eb5f6cc841e5a34e6bfc574825
gkekd1325 <gkekd1325@naver.com>
2022-05-10 22:39:41 +0900

    update

--- pokachip/js/script.js
+++ pokachip/js/script.js
@@ -5,6 +5,26 @@ function trans(){
     $(".head li").addClass("on");
     $(".logo").addClass("on");
 }
+var original=document.querySelector(".original");
+var onion=document.querySelector(".onion");
 function scroll(){
     console.log(scrollY);
+    if(scrollY>=390){
+        onion.style.transform="translateX(0)";
+        original.style.transform="translateX(0)";
+    }
+    else{
+        onion.style.transform="translateX(100%)";
+        original.style.transform="translateX(-100%)";
+    }
+    if(innerWidth>=800){
+        if(scrollY>=2700){
+            onion.style.transform="translateX(0)";
+            original.style.transform="translateX(0)";
+        }
+        else{
+            onion.style.transform="translateX(100%)";
+            original.style.transform="translateX(-100%)";
+        }
+    }
 }
